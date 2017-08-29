19f109e3a5762d20c67b80cab8d8947e4fb05147
Despairon <MaxDespairon@gmail.com>
2017-08-29 20:52:06 +0300

    added missed untracked files

---
+++ Assets/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.W))
+            transform.Translate(-0.1f,0,0);
+        if (Input.GetKey(KeyCode.S))
+            transform.Translate(0.1f, 0, 0);
+        if (Input.GetKey(KeyCode.A))
+            transform.Translate(0, 0, -0.1f);
+        if (Input.GetKey(KeyCode.D))
+            transform.Translate(0, 0, 0.1f);
+    }
+}
