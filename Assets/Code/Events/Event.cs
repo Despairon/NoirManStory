a500bcf795bdae4cde9254117e3e3228e192ec04
Despairon <MaxDespairon@gmail.com>
2017-10-26 21:49:20 +0300

    events manager implementation started.

---
+++ Assets/Code/Events/Event.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public enum EventID
+{
+    NONE,
+};
+
+public class EventData
+{
+    // TODO: ...
+}
+
+public delegate void EventHandlers(Enum eventID, EventData eventData);
+
+public class Event
+{
+    #region private_members
+
+    private Enum          _eventID;
+    private EventData     _eventData;
+    private EventHandlers _eventHandlers;
+
+    #endregion
+
+    #region public_members
+
+    public void GC()
+    {
+       
+    }
+
+    #endregion
+}
---
+++ Assets/Code/Events/EventsManager.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class EventsManager
+{
+    #region private_members
+
+    private enum EventBroadcastType
+    {
+        NONE,
+        SEND_EVENT_BY_OBJECT_NAME,
+        BROADCAST_BY_TAG,
+        BROADCAST_ALL
+    };
+
+    private Queue<object>        _eventQueue;
+
+    private static EventsManager _instance;
+    private static object        _mutex = new object();
+
+    #endregion
+
+    #region public_members
+
+    public static EventsManager instance
+    {
+        get
+        {
+            lock (_mutex)
+            {
+                if (_instance == null)
+                    _instance = new EventsManager();
+            }
+
+            return _instance;
+        }
+    }
+
+    #endregion
+
+    public EventsManager()
+    {
+        _eventQueue = new Queue<object>();
+    }
+
+    public void sendEventToObject(string objectName, EventID eventID, EventData eventData)
+    {
+        // TODO: sendEventToObject: implement
+    }
+
+    public void broadcastEventByTag(string tag, EventID eventID, EventData eventData)
+    {
+        // TODO: broadcastEventByTag: implement
+    }
+
+    public void broadcastEvent(EventID eventID, EventData eventData)
+    {
+        // TODO: broadcastEvent: implement
+    }
+
+
+}
